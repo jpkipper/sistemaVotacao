0c4519b65c3ae9ec1656ad67bb8549e8da89c2c2
João P. Kipper <jpkipper@hotmail.com>
2024-06-01 10:39:24 -0300

    feat(ENUMS): Adicionado enums de votos e status associado

---
+++ sistema-votacao/src/main/java/br/com/db/sistema/votacao/v1/enums/AssociateStatusEnum.java
@@ -0,0 +1,19 @@
+package br.com.db.sistema.votacao.v1.enums;
+
+public enum AssociateStatusEnum 
+{
+    ABLE_TO_VOTE( "ABLE_TO_VOTE" ),
+    UNABLE_TO_VOTE( "UNABLE_TO_VOTE" );
+
+    private final String value;
+
+    AssociateStatusEnum( String value )
+    {
+        this.value = value;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+}
---
+++ sistema-votacao/src/main/java/br/com/db/sistema/votacao/v1/enums/VoteEnum.java
@@ -0,0 +1,19 @@
+package br.com.db.sistema.votacao.v1.enums;
+
+public enum VoteEnum 
+{
+    SIM( "SIM" ),
+    NÃO( "NÃO" );
+
+    private final String value;
+
+    VoteEnum( String vote )
+    {
+        this.value = vote;
+    }
+
+    public String getValue()
+    {
+        return value;
+    }
+}
